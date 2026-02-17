2289e637f7ae9aece3ed8ab1c0d17032f34ca646
alonzojoe <alonzojoenell2k15@gmail.com>
2026-02-17 11:53:05 +0800

    Join actor room on socket connect

--- src/shared/api/chatSocket.ts
+++ src/shared/api/chatSocket.ts
@@ -12,6 +12,7 @@ export function createChatSocket(input: { role: ActorRole; actorId: string }) {
   socket.on('connect', () => {
     // eslint-disable-next-line no-console
     console.log('[chat-socket] connected', socket.id);
+    socket.emit('join:actor', { role: input.role, actorId: input.actorId });
   });
   socket.on('connect_error', (err) => {
     // eslint-disable-next-line no-console
